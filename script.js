cb572497e3af728afb9837ccd1f469719942abad
shibamdey269 <31677365-shibamdey269@users.noreply.replit.com>
2024-02-02 00:25:54 +0000

    Lol

--- script.js
+++ script.js
@@ -4,20 +4,18 @@ function refreshPage() {
 }
 function savePeriodData() {
   const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
   const startDate = document.getElementById("startDate").value;
   const endDate = document.getElementById("endDate").value;
   const bloodColor = document.getElementById("bloodColor").value;
-  if (!name || !email || !startDate || !endDate || !bloodColor)
+  if (!name || !startDate || !endDate || !bloodColor)
     return alert("Please fill in all fields.");
   const periodData = {
-    name: name,
-    email: email,
+    name,
     data: [
       {
-        startDate: startDate,
-        endDate: endDate,
-        bloodColor: bloodColor,
+        startDate,
+        endDate,
+        bloodColor,
       },
     ],
   };
@@ -41,7 +39,6 @@ console.log(savedData);
     savedDataContainer.innerHTML += `
     <p>
 <strong>Name: ${data.name}</strong><br>
-      Email: ${data.email}<br>
       Start Date: ${res.startDate}<br>
       End Date: ${res.endDate}<br>
       Blood Color: ${res.bloodColor}<br>
@@ -73,7 +70,6 @@ function editData(index) {
   let periodData = JSON.parse(localStorage.getItem("periodData")) || [];
       const data = periodData[index];
       document.getElementById('name').value = data.name;
-      document.getElementById('email').value = data.email;
       document.getElementById('startDate').value = data.data[0].startDate;
       document.getElementById('endDate').value = data.data[0].endDate;
       document.getElementById('bloodColor').value = data.data[0].bloodColor;
